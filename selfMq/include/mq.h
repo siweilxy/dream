41170ab0989611b915a658151ce46465afc8e191
siweilxy <siweilxy@163.com>
2018-05-27 22:49:29 +0800

    add mq

---
+++ selfMq/include/mq.h
@@ -0,0 +1,14 @@
+//
+// Created by siwei on 18-5-27.
+//
+
+#ifndef DREAM_MQ_H
+#define DREAM_MQ_H
+
+
+class mq {
+
+};
+
+
+#endif //DREAM_MQ_H
---
+++ selfMq/src/mq.cpp
@@ -0,0 +1,5 @@
+//
+// Created by siwei on 18-5-27.
+//
+
+#include "mq.h"
